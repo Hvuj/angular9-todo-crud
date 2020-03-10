05acc6bb1cb1f87a72bcc885258286582d4858c0
Hvuj <56831016+Hvuj@users.noreply.github.com>
2020-03-10 15:20:52 +0200

    angular 9 crud httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AddIssueComponent } from "./components/add-issue/add-issue.component";
+import { EditIssueComponent } from "./components/edit-issue/edit-issue.component";
+import { IssueListComponent } from "./components/issue-list/issue-list.component";
+import { BugService } from "shared/bug.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [BugService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
